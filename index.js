8aa82c7fe314fbb21daa43cdbd4f1a9b809bd987
Ali Irani <aliirani74@gmail.com>
2017-08-03 22:25:36 +0430

    polling for every chat fixed

--- index.js
+++ index.js
@@ -14,12 +14,96 @@ bot.onText(/\/start/, (msg) => {
             "keyboard": [["ساخت فرم"]]
         }
     }) */
-    id = msg.chat.id
-    feed[id] = []
-    bot.sendMessage(id, id)
+
+    feed[msg.chat.id] = []
+    createPolling(msg.chat.id);
 })
 
 
+function createPolling(index) {
+    var polling = AsyncPolling(function (end) {
+        console.log('index', index)
+        req = request('http://goings0lo.mihanblog.com/post/rss/')
+        var feedparser = new FeedParser([]);
+        console.log('new request')
+
+        req.on('error', function (error) {
+            end('error on request')
+            //bot.sendMessage(id, 'error on request')
+        });
+
+        req.on('response', function (res) {
+            var stream = this; // `this` is `req`, which is a stream
+
+            if (res.statusCode !== 200) {
+                bot.sendMessage(index, 'err'+str(new Error('Bad status code')))
+            }
+            else {
+                stream.pipe(feedparser);
+                end(null, {'feedparser': feedparser,
+                    'index': index})
+            }
+        });
+
+
+    }, 10000);
+
+    polling.on('error', function (error) {
+        console.log('in polling err')
+    });
+    polling.on('result', function (result) {
+        console.log('in result')
+        feedparser = result.feedparser
+        chatId = result.index
+        console.log('chatId', chatId)
+
+        feedparser.on('error', function (error) {
+            bot.sendMessage(chatId, 'feedparser err')
+            // always handle errors
+        });
+
+        feedparser.on('readable', function () {
+            //console.log('readble')
+            // This is where the action is!
+            var stream = this; // `this` is `feedparser`, which is a stream
+            var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+            var item;
+
+            while (item = stream.read()) {
+                //bot.sendMessage(id, '10')
+                var link = item.link
+                if(feed[chatId].find(function (e) {
+                        return e == link
+                    }) != undefined) {
+                    continue
+                }
+                feed[chatId].push(item.link)
+
+                var text = htmlToText.fromString(item.description, {
+                    wordwrap: null
+                })
+                var messageNumber = Math.ceil(text.length / 4096.0)
+                for(var i = 0; i < messageNumber; i++) {
+                    var sendText
+                    if(text.length - i*4096 < 4096) {
+                        sendText = text.slice(i*4096, text.length - i*4096)
+                    } else {
+                        sendText = text.slice(i*4096, (i+1)*4096)
+                    }
+                    try {
+                        if(sendText.length > 0)
+                            bot.sendMessage(chatId, sendText)
+                    }
+                    catch (error) {
+                        console.log("Something went wrong: ", error);
+                    }
+                }
+            }
+        });
+    });
+
+    polling.run()
+}
 
 var FeedParser = require('feedparser');
 var request = require('request'); // for fetching the feed
@@ -27,86 +111,11 @@ var htmlToText = require('html-to-text');
 var AsyncPolling = require('async-polling');
 
 
-var req = request('http://beforethedawn.blogfa.com/rss')
 
 
-var polling = AsyncPolling(function (end) {
-    console.log(id)
-    console.log(feed[id])
-    req = request('http://goings0lo.mihanblog.com/post/rss/')
-    var feedparser = new FeedParser([]);
-    console.log('new request')
-
-    req.on('error', function (error) {
-        end('error on request')
-        //bot.sendMessage(id, 'error on request')
-    });
-
-    req.on('response', function (res) {
-        var stream = this; // `this` is `req`, which is a stream
-
-        if (res.statusCode !== 200) {
-            bot.sendMessage(id, 'err'+str(new Error('Bad status code')))
-        }
-        else {
-            stream.pipe(feedparser);
-            end(null, feedparser)
-        }
-    });
 
 
-}, 10000);
-
-polling.on('error', function (error) {
-    console.log('in polling err')
-});
-polling.on('result', function (feedparser) {
-    console.log('in result')
 
 
-    feedparser.on('error', function (error) {
-        bot.sendMessage(id, 'feedparser err')
-        // always handle errors
-    });
 
-    feedparser.on('readable', function () {
-        //console.log('readble')
-        // This is where the action is!
-        var stream = this; // `this` is `feedparser`, which is a stream
-        var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-        var item;
-
-        while (item = stream.read()) {
-            //bot.sendMessage(id, '10')
-            var link = item.link
-            if(feed[id].find(function (e) {
-                    return e == link
-                }) != undefined) {
-                continue
-            }
-            feed[id].push(item.link)
-
-            var text = htmlToText.fromString(item.description, {
-                wordwrap: null
-            })
-            var messageNumber = Math.ceil(text.length / 4096.0)
-            for(var i = 0; i < messageNumber; i++) {
-                var sendText
-                if(text.length - i*4096 < 4096) {
-                    sendText = text.slice(i*4096, text.length - i*4096)
-                } else {
-                    sendText = text.slice(i*4096, (i+1)*4096)
-                }
-                try {
-                    if(sendText.length > 0)
-                        bot.sendMessage(id, sendText)
-                }
-                catch (error) {
-                    console.log("Something went wrong: ", error);
-                }
-            }
-        }
-    });
-});
-
-polling.run(); // Let's start polling.
+ // Let's start polling.
